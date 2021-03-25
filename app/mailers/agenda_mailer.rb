40bd333edf310ccd3a1c4aba3ffcc9841a253270
yuuki kajiyama <luv4u.1331@gmail.com>
2021-03-25 16:18:57 +0900

    feat.:メールのview修正

--- app/mailers/agenda_mailer.rb
+++ app/mailers/agenda_mailer.rb
@@ -1,7 +1,7 @@
 class AgendaMailer < ApplicationMailer
   def send_mail(user)
     @user = user
-    mail(to: user.email, subject: "Agenda削除通知")
+    mail(to: @user.email, subject: "Agenda削除通知")
   end
 
   def self.send_mail_users(users)
